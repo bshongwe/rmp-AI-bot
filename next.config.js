e8d5d03af0a4ce9c6108608026b454cad5bf5b23
Ernest Bhekizwe Shongwe <126109055+bshongwe@users.noreply.github.com>
2024-09-11 00:57:45 +0200

    Revert "Integrated Openrouter model"

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // (Optional) Export as a standalone site
-  // See https://nextjs.org/docs/pages/api-reference/next-config-js/output#automatically-copying-traced-files
-  output: 'standalone', // Feel free to modify/remove this option
-
-  // Indicate that these packages should not be bundled by webpack
-  experimental: {
-    serverComponentsExternalPackages: ['sharp', 'onnxruntime-node'],
-  },
-};
-
+  reactStrictMode: true,
+  swcMinify: true,
+}
 
 module.exports = nextConfig
